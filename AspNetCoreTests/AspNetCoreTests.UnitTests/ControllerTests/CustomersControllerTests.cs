4622b7d7f0269f4ab9c18af898556c0da5093e82
Gunnar Peipman <gpeipman@hotmail.com>
2022-05-06 07:33:07 +0300

    added unit tests for customers controller edit action

--- AspNetCoreTests/AspNetCoreTests.UnitTests/ControllerTests/CustomersControllerTests.cs
+++ AspNetCoreTests/AspNetCoreTests.UnitTests/ControllerTests/CustomersControllerTests.cs
@@ -125,5 +125,81 @@ namespace AspNetCoreTests.UnitTests.ControllerTests
             Assert.NotNull(result);
             Assert.Equal("Index", result.ActionName);
         }
+
+        [Fact]
+        public async Task Edit_should_return_edit_view_when_customer_was_found()
+        {
+            // Arrange
+            var viewNames = new[] { null, "Edit" };
+            var id = 1;
+            var model = new CustomerModel { Id = id };
+            _customerServiceMock.Setup(service => service.GetCustomer(id))
+                                .ReturnsAsync(model);
+
+            // Act
+            var result = await _controller.Edit(id) as ViewResult;
+            var viewName = result.ViewName;
+
+            // Assert
+            Assert.Contains(viewName, viewNames);
+            Assert.Equal(model, result.Model);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_not_found_when_customer_was_not_found()
+        {
+            // Arrange
+            var id = 1;
+            var model = (CustomerModel)null;
+            _customerServiceMock.Setup(service => service.GetCustomer(id))
+                                .ReturnsAsync(model);
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_bad_request_when_id_is_missing()
+        {
+            // Arrange
+            var id = (int?)null;
+
+            // Act
+            var result = await _controller.Edit(id) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_bad_request_for_null_model()
+        {
+            // Arrange
+            var model = (CustomerModel)null;
+
+            // Act
+            var result = await _controller.Edit(model) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_to_index_after_successful_save()
+        {
+            // Arrange
+            var model = new CustomerModel();
+            _customerServiceMock.Setup(c => c.SaveCustomer(model));
+
+            // Act
+            var result = await _controller.Create(model) as RedirectToActionResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Index", result.ActionName);
+        }
     }
 }
